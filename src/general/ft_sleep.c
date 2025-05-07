6863d363c9c8b9adfe09b4f33a811278afee4086
Issa Oulkhir <aissaoulkhir22@gmail.com>
2025-05-07 11:23:11 +0100

    back to skeleton

--- src/general/ft_sleep.c
+++ src/general/ft_sleep.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 08:41:55 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/05/02 13:38:53 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/05/07 11:21:30 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	ft_sleep(t_philo *philo, long duration)
 	long	then;
 
 	then = get_time_now();
-	while (get_time_now() < then + duration && !safe_get_death_flag(philo))
+	while (get_time_now() < then + duration)// && !safe_get_death_flag(philo))
 		usleep(100);
 }
--- src/simulation/start_simulation.c
+++ src/simulation/start_simulation.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:57:38 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/05/07 11:19:27 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/05/07 11:21:02 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	start_simulation(t_my_data data)
 {
-	printf("%d\n", data.philos_num);
-	printf("%d\n", data.time_to_die);
-	printf("%d\n", data.time_to_eat);
-	printf("%d\n", data.time_to_sleep);
-	printf("%d\n", data.times_to_eat);
+	printf("%ld\n", data.philos_num);
+	printf("%ld\n", data.time_to_die);
+	printf("%ld\n", data.time_to_eat);
+	printf("%ld\n", data.time_to_sleep);
+	printf("%ld\n", data.times_to_eat);
 	return (EXIT_SUCCESS);
 }
