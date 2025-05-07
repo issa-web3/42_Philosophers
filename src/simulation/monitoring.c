9fc2ff549ad3afec05426c10953307e021780db9
Issa Oulkhir <aissaoulkhir22@gmail.com>
2025-05-07 20:21:37 +0100

    fixes

--- src/simulation/monitoring.c
+++ src/simulation/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:57:38 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/05/07 19:57:25 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/05/07 20:18:08 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ int		death_or_all_eaten(t_broadcasted_info *info)
 		if (dad_or_alive(&philos[i], info->data))
 			return (print_action(&philos[i], "died"), YES);
 		all_eaten &= eaten_enough(&philos[i], info->data);
+		usleep(10);
 		i++;
 	}
 	return (all_eaten);
--- src/simulation/routine_func.c
+++ src/simulation/routine_func.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine_func.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: test <test@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:57:38 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/05/07 17:23:13 by test             ###   ########.fr       */
+/*   Updated: 2025/05/07 20:16:03 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,12 @@ void	eating(t_philo *self)
 	sort_mutexes(&r_fork, &l_fork);
 	pthread_mutex_lock(r_fork);
 	print_action(self, "has taken a fork");
-	// check case num = 1
+	if (r_fork == l_fork)
+	{
+		pthread_mutex_unlock(r_fork);
+		ft_sleep(self, self->info->data.time_to_die);
+		return ;
+	}
 	pthread_mutex_lock(l_fork);
 	print_action(self, "has taken a fork");
 	print_action(self, "is eating");
