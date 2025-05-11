b2cbfea91eda3e78ea4a0a7160f7bf720239f6e2
Issa Oulkhir <ioulkhir@e1r8p15.1337.ma>
2025-05-11 13:21:14 +0100

    no problem

--- mutex_safe.c
+++ mutex_safe.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:57:38 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/05/09 16:29:59 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/05/11 13:18:57 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,7 @@ void	print_action(t_philo *self, char *action)
 	pthread_mutex_lock(&self->info->printing);
 	if (safe_getter_setter(&self->info->death, GET, 314) == NOT_YET)
 		printf("%ld %d %s\n", get_time_now(), self->id, action);
+	if (*action == 'd')
+		safe_getter_setter(&self->info->death, SET, YES);
 	pthread_mutex_unlock(&self->info->printing);
 }
